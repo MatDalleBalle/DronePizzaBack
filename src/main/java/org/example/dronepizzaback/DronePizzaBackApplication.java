05af619c1e02e498047623b9760bf928f49be3b2
MatDalleBalle <mada0003@stud.kea.dk>
2025-01-16 11:00:09 +0100

    Initial commit

---
+++ src/main/java/org/example/dronepizzaback/DronePizzaBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.dronepizzaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronePizzaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronePizzaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dronepizzaback/DronePizzaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dronepizzaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronePizzaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
